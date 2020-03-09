03c143cf026a5029c82a751d65ffc6c22cb9a66a
DakotaLMartinez <dakotaleemusic@gmail.com>
2020-03-09 16:48:26 -0700

    add albums

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_09_233507) do
+ActiveRecord::Schema.define(version: 2020_03_09_234654) do
+
+  create_table "albums", force: :cascade do |t|
+    t.string "title"
+    t.string "artist_name"
+    t.string "image_url"
+    t.string "mbid"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
